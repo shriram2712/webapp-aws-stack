a5acdf15ccb3d389aa279da25c4e61aee78e04d4
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2024-01-24 13:34:10 -0800

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -31,7 +31,19 @@ variable "instance_ami_id" {
 }
 
 variable "user_data" {
-  default = "./ubto_userdata.txt"
+  default = "<<-EOT
+    #!/bin/sh
+    sudo apt-get update
+    sudo apt --assume-yes install nginx
+    sudo apt --assume-yes install git
+    sudo ufw allow 'Nginx HTTP'
+    #sudo systemctl start nginx
+    sudo systemctl enable nginx
+    sudo systemctl status nginx
+    sudo git clone https://github.com/omeeomi/tf-cloud-demo-website /tmp/tf-cloud-demo-website
+    sudo mkdir -p /var/www/html/
+    sudo cp /tmp/tf-cloud-demo-website/index.html /var/www/html/index.html
+    EOT" 
 }
 
 variable "ebs_volume_enabled" {
