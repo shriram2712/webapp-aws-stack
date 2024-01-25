f18318c0369feea3a80ae607e339257967c6e45c
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2024-01-24 20:42:21 -0800

    Moved it back to a variable

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -31,19 +31,7 @@ variable "instance_ami_id" {
 }
 
 variable "user_data" {
-  default =<<-EOT
-    #!/bin/sh
-    sudo apt-get update
-    sudo apt --assume-yes install nginx
-    sudo apt --assume-yes install git
-    sudo ufw allow 'Nginx HTTP'
-    #sudo systemctl start nginx
-    sudo systemctl enable nginx
-    sudo systemctl status nginx
-    sudo git clone https://github.com/omeeomi/tf-cloud-demo-website /tmp/tf-cloud-demo-website
-    sudo mkdir -p /var/www/html/
-    sudo cp /tmp/tf-cloud-demo-website/index.html /var/www/html/index.html
-    EOT
+  default = filebase64("${path.module}/ubto_userdata.txt")
 }
 
 variable "ebs_volume_enabled" {
